3e83cf1ecd85ce483deafa2d59df44287953ed9f
dimon4eg212 <dimoncheg.adv@gmail.com>
2015-09-11 22:19:12 +0300

    Install Bootstrap, setting routing, add option remove only confirm

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FDevs\CRUDBundle\FDevsCRUDBundle(),
+            new Twitter\BootstrapBundle\TwitterBootstrapBundle()
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
